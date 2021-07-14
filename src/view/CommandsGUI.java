710b38ba594eaec716b8c0065bf3b128cdb27a06
RonaldJ11 <47287333+RonaldJ11@users.noreply.github.com>
2021-07-13 22:26:31 -0500

    Update CommandsGUI.java

--- src/view/CommandsGUI.java
+++ src/view/CommandsGUI.java
@@ -1,5 +1,9 @@
 package view;
-
+/**
+ * Comandos utilzados para escuchar los botones.
+ * @version 1.2, 24/12/04
+ * @author Rafa Caballero
+ */
 public enum CommandsGUI {
     CREATE_GRAMMAR, ANALIZE_WORD;
 }
